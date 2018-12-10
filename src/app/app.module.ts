116c472f5f3e041c3aaffbccace3a359b73cefa5
Guest User <Guest@epicodus-94.local>
2018-12-10 11:58:21 -0800

    add top-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
